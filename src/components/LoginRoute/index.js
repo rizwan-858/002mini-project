bda03c29f76b57e12eefe31f17cdc91ad6e431e2
rizwan <rizwanpathan858@gmail.com>
2021-10-15 16:27:13 +0000

    mini project

--- src/components/LoginRoute/index.js
+++ src/components/LoginRoute/index.js
@@ -8,113 +8,122 @@ class Login extends Component {
   state = {
     username: '',
     password: '',
-    showError: false,
+    showSubmitError: false,
     errorMsg: '',
   }
 
-  onLoginSuccess = token => {
-    Cookies.set('jwt_token', token, {
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  onSubmitSuccess = jwtToken => {
+    const {history} = this.props
+
+    Cookies.set('jwt_token', jwtToken, {
       expires: 30,
       path: '/',
     })
-    const {history} = this.props
     history.replace('/')
   }
 
-  onLoginFailure = msg => {
-    this.setState(prevState => ({
-      errorMsg: msg,
-      showError: !prevState.showError,
-    }))
+  onSubmitFailure = errorMsg => {
+    console.log(errorMsg)
+    this.setState({showSubmitError: true, errorMsg})
   }
 
-  onSubmitForm = async event => {
+  submitForm = async event => {
     event.preventDefault()
     const {username, password} = this.state
     const userDetails = {username, password}
-    const apiUrl = 'https://apis.ccbp.in/login'
+    const url = 'https://apis.ccbp.in/login'
     const options = {
       method: 'POST',
       body: JSON.stringify(userDetails),
     }
-    const response = await fetch(apiUrl, options)
+    const response = await fetch(url, options)
     const data = await response.json()
     if (response.ok === true) {
-      this.onLoginSuccess(data.jwt_token)
+      this.onSubmitSuccess(data.jwt_token)
     } else {
-      this.onLoginFailure(data.error_msg)
+      this.onSubmitFailure(data.error_msg)
     }
   }
 
-  onEnterUsername = event => {
-    this.setState({username: event.target.value, showError: false})
+  renderPasswordField = () => {
+    const {password} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="password-input-field"
+          value={password}
+          onChange={this.onChangePassword}
+        />
+      </>
+    )
   }
 
-  onEnterPassword = event => {
-    this.setState({password: event.target.value, showError: false})
+  renderUsernameField = () => {
+    const {username} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="username-input-field"
+          value={username}
+          onChange={this.onChangeUsername}
+        />
+      </>
+    )
   }
 
   render() {
-    const {showError, errorMsg, username, password} = this.state
+    const {showSubmitError, errorMsg} = this.state
     const jwtToken = Cookies.get('jwt_token')
     if (jwtToken !== undefined) {
       return <Redirect to="/" />
     }
     return (
-      <div className="loginMain">
-        <div className="small">
-          <h1 className="smallLoginName">Login</h1>
-          <img
-            className="smallLander"
-            alt="website logo"
-            src="https://res.cloudinary.com/rizwan987/image/upload/v1633272580/Rectangle_1457_m5lm0c.png"
-          />
+      <div className="login-form-container">
+        <div className="form-sub-Container">
+          <form className="form-container" onSubmit={this.submitForm}>
+            <img
+              src="https://res.cloudinary.com/rizwan987/image/upload/v1633174126/Frame_274_arvvzt.png"
+              className="login-website-logo-desktop-image"
+              alt="website logo"
+            />
+            <img
+              className="small-screen-logo"
+              src="https://res.cloudinary.com/rizwan987/image/upload/v1633272580/Rectangle_1457_m5lm0c.png"
+              alt="website logo"
+            />
+            <h1 className="heading">Tasty Kitchens</h1>
+            <h1 className="LoginHeading">Login</h1>
+            <div className="input-container">{this.renderUsernameField()}</div>
+            <div className="input-container">{this.renderPasswordField()}</div>
+            <button type="submit" className="login-button">
+              Login
+            </button>
+            {showSubmitError && <p className="error-message">*{errorMsg}</p>}
+          </form>
         </div>
-        <div className="loginCon">
-          <div className="formCon">
-            <div className="logoCon">
-              <img
-                alt="website logo"
-                src="https://res.cloudinary.com/rizwan987/image/upload/v1633174126/Frame_274_arvvzt.png"
-              />
-              <h1 className="logoName">Tasty Kitchens</h1>
-              <h1 className="loginName">Login</h1>
-            </div>
-            <form onSubmit={this.onSubmitForm}>
-              <div className="inputCon">
-                <label htmlFor="username">USERNAME</label>
-                <input
-                  onChange={this.onEnterUsername}
-                  className="input"
-                  value={username}
-                  type="input"
-                  id="username"
-                />
-              </div>
-              <div className="inputCon">
-                <label htmlFor="password">PASSWORD</label>
-                <input
-                  onChange={this.onEnterPassword}
-                  className="input"
-                  value={password}
-                  type="password"
-                  id="password"
-                />
-              </div>
-              {showError ? <p className="errorMsg">{errorMsg}</p> : null}
-              <button className="loginBtn" type="submit">
-                Login
-              </button>
-            </form>
-          </div>
-        </div>
-        <div className="lander">
-          <img
-            className="landerImg"
-            alt="website login"
-            src="https://res.cloudinary.com/rizwan987/image/upload/v1633170425/Rectangle_1456_gmu29p.png"
-          />
-        </div>
+        <img
+          src="https://res.cloudinary.com/rizwan987/image/upload/v1633170425/Rectangle_1456_gmu29p.png"
+          className="websiteImage"
+          alt="website login"
+        />
       </div>
     )
   }
